8c69564f98780376992fc5ee1ef64040ba73d151
Andrew <andrewlyuk@gmail.com>
2025-03-30 20:02:20 +0800

    updated wokwi scheme

--- save-to-usb/wokwi/sketch.ino
+++ save-to-usb/wokwi/sketch.ino
@@ -2,47 +2,20 @@
 #include <Tiny4kOLED.h>
 #include <GyverButton.h>
 
-#define SCREEN_WIDTH  128
-#define SCREEN_HEIGHT 64
-#define STORE_SIZE 2   // Reduced to save memory
+// ===========================================================================
 
-#define MOD_CHANGE_PIN PB4
-#define ADC_READ_PIN PB3
+#define SCREEN_WIDTH  128 // Ширина экрана в точках
+#define SCREEN_HEIGHT 64  // Высота экрана в точках
 
-typedef void (*generatorFunc)(uint8_t*);
+#define ADC_READ_PIN  PB3 // Пин пробы замеров
 
-class Grapher {
-  private:
-      uint8_t data[SCREEN_WIDTH];  // Only one screen data buffer
+#define STORE_BTN_PIN PB1 // Пин для управление памтью
+#define SCALE_BTN_PIN PB4 // Пин масштабирования изображения
+#define MODE_BTN_PIN  PB5 // Пин режима
 
-  public:
-      Grapher() { memset(data, 0, sizeof(data)); }
+#define STORE_SIZE    1
 
-      void apply(generatorFunc genFunc) {genFunc(data); }
-
-      void display() {
-          oled.clear(); 
-          for (uint8_t x = 0; x < SCREEN_WIDTH; x++) {
-
-              uint8_t y = uint8_t(SCREEN_HEIGHT) - data[x];
-
-              // overflow check ====
-              if (y >= SCREEN_HEIGHT) continue;
-
-              // page calc =========
-              uint8_t page = y / 8;
-              uint8_t bit = y % 8;
-
-              // ==================
-              oled.setCursor(x, page);
-              oled.startData();
-              oled.sendData(1 << bit);
-              oled.endData();
-          }
-      }
-
-      uint8_t* getData() { return data; }
-};
+// ===========================================================================
 
 class MilTimer {
   private:
@@ -52,76 +25,141 @@ class MilTimer {
       MilTimer(unsigned long time) : intervalTime(time) {}
       bool isDone() {
           unsigned long now = millis();
-          if (now - tNext >= intervalTime) {
-              tNext = now;
-              return true;
-          }
-          return false;
+
+          boolean state = now - tNext >= intervalTime;
+          if (state) tNext = now;
+
+          return state;
       }
 };
 
-class MemStore {
-  private:
-      uint8_t storeArr[STORE_SIZE][SCREEN_WIDTH];  // Reduced store size to 2 for less memory usage
-      uint8_t currentIndex = 0;
+// ===========================================================================
 
-  public:
-      uint8_t* getArr() { return storeArr[currentIndex]; }
+uint8_t GraphicArray[SCREEN_WIDTH];
+volatile uint8_t scaleFactor = 1;
 
-      void next() { if (++currentIndex >= STORE_SIZE) currentIndex = 0; }
+uint8_t SaveArray[STORE_SIZE][SCREEN_WIDTH];
+int SaveArrShift = 0;
 
-      void push(uint8_t* src) { memcpy(storeArr[currentIndex], src, SCREEN_WIDTH); }
-};
+boolean saveState = false;
+boolean viewMode = false;
 
-Grapher graph;
-MilTimer timer1s(1000);
-MemStore store;
-GButton modeBtn(MOD_CHANGE_PIN);
-// GButton ctrlBtn(CTRL_PIN);
-bool streamMode = true;
+MilTimer time(1500); // Таймер для измерений
+
+// Установки парамтеров кнопок управления
+GButton modeBtn(MODE_BTN_PIN);
+GButton storeBtn(STORE_BTN_PIN);
+GButton scaleBtn(SCALE_BTN_PIN);
+
+// ===========================================================================
 
 void setup() {
-    pinMode(PB1, OUTPUT);
-    oled.begin(128, 64, sizeof(tiny4koled_init_128x64br), tiny4koled_init_128x64br);
+    oled.begin(
+		128, 64,
+		sizeof(tiny4koled_init_128x64br),
+		tiny4koled_init_128x64br
+	);
     oled.clear();
     oled.on();
+
+	modeBtn.setTimeout(500);
+	storeBtn.setTimeout(500);
+  scaleBtn.setTimeout(500);
 }
 
 void loop() {
-    modeBtn.tick();
-    // ctrlBtn.tick();
+	modeBtn.tick();
+	storeBtn.tick();
+	
+	if (scaleBtn.isSingle()) scaleFactor++; // Масштабируем ихображение
+  if (scaleBtn.isHold()) scaleFactor = 1; // Сброс масштаба на экране
 
+  // Смена режима отображения из памяти или чтения канала
+	if (modeBtn.isSingle()) {
+		viewMode = !viewMode;
+		return;
+	}
+  
+  // Переключаем секцию памяти через кольцевой буфер
+  if (modeBtn.isSingle()) {
+		SaveArrShift = (SaveArrShift + 1) % STORE_SIZE;
+		return;
+	}
 
-    // if (ctrlBtn.isSingle()) {
-    //     graph.apply(readTemp);
-    //     graph.display();
-    // }
+  // Сохраняем уже отображаемый фрейм с графиком в буфер, если стоит режим чтения канала
+  if (storeBtn.isHold() && !viewMode) {
+		saveState = true;
+		return;
+	}
+	
+  // Проверка на режим чтения памяти, если да, то делаем отображение по запросу
+	if (viewMode) {
+		if (storeBtn.isSingle()) {
+			readSavedADC(GraphicArray);
+			oledDisplay(GraphicArray, scaleFactor, false);
+		}
+		return;
+	}
 
-    if (modeBtn.isHold()) store.next();
-    if (modeBtn.isSingle()) {
-      digitalWrite(PB1, HIGH);
-      streamMode = !streamMode;
-      // if (!streamMode) {
-      //   graph.apply(readTemp);
-      //   graph.display();
-      // }
-    }
+  // Стандартное отображение в графика
+	if (time.isDone())
+	{	
+		if (!saveState) readADC(GraphicArray);
+		oledDisplay(GraphicArray, scaleFactor, saveState);
+    saveState = false;
+	}
+}
 
-    if (timer1s.isDone() && streamMode) {
-        graph.apply(osciReadLine);
-        graph.display();
-        store.push(graph.getData());
+// ===========================================================================
+
+// readSavedADC - читает из памяти со сдвигом в буфер для экрана
+void readSavedADC(uint8_t *arr) {
+	for (uint8_t x = 0; x < SCREEN_WIDTH; x++)
+	{
+		arr[x] = SaveArray[SaveArrShift][x];
+	}
+}
+
+// readADC - формирует массив значений ADC
+void readADC(uint8_t *arr) {
+	int mappedValue;
+	
+    for (uint8_t x = 0; x < SCREEN_WIDTH; x++) {
+	    int value = analogRead(ADC_READ_PIN);
+	    
+	    if (value <= 512) {
+	        mappedValue = map(value, 0, 512, 63, 31);
+	    } else {
+	        mappedValue = map(value, 512, 1023, 31, 0);
+	    }
+	
+      	arr[x] = uint8_t(mappedValue);
     }
 }
 
-void osciReadLine(uint8_t *arr) {
-    for (uint8_t x = 0; x < SCREEN_WIDTH; x++) {
-        arr[x] = analogRead(ADC_READ_PIN) - 1;
-    }
-}
-
-void readTemp(uint8_t *arr) {
-    for (uint8_t x = 0; x < SCREEN_WIDTH; x++) {
-        arr[x] = store.getArr()[x];
-    }
+// oledDisplay - отобрпажаем массив в память
+void oledDisplay(uint8_t* valueArr, uint8_t scale, boolean isSave) {
+	oled.clear();
+	for (uint8_t x = 0; x < SCREEN_WIDTH; x++) {
+	
+		uint8_t pos = x / scale;
+		
+    // Сохрканение отображаемого массива в память по инедксам
+		if (isSave) SaveArray[SaveArrShift][x] = GraphicArray[pos];
+	
+	  // Вычисление страницы и бита
+		// Ограничиваем значение от 0 до 63
+		uint8_t value = (valueArr[pos] > 63) ? 63 : valueArr[pos];
+		
+	    uint8_t page = value / 8;    // Определяем страницу (каждая страница 8 пикселей)
+	    uint8_t shiftBit = 1 << (value % 8);     // Определяем бит в странице
+	
+		// Определеяем страницу пространства экрана
+	    oled.setCursor(x, page);
+	
+		// Устанавливаем курсор в нужную позицию
+	    oled.startData();
+	    oled.sendData(shiftBit);
+	    oled.endData();
+	}
 }
