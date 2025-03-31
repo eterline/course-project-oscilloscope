90baecafbed8c0fef3660c97043d2a16cc12b2c5
Andrew <andrewlyuk@gmail.com>
2025-03-31 21:33:17 +0800

    updates schemes

--- oscill-simulide/main.ino
+++ oscill-simulide/main.ino
@@ -1,28 +1,34 @@
 #include <TinyWireM.h>
 #include <Tiny4kOLED.h>
 #include <GyverButton.h>
+#include <EEPROM.h>
 
 // ===========================================================================
 
-#define SCREEN_WIDTH  128 // Ширина экрана в точках
-#define SCREEN_HEIGHT 64  // Высота экрана в точках
+#define SCREEN_WIDTH  	128 // Ширина экрана в точках
+#define SCREEN_HEIGHT 	64  // Высота экрана в точках
 
-#define ADC_READ_PIN  PB3 // Пин пробы замеров
+#define ADC_READ_PIN  	PB3 // Пин пробы замеров
 
-#define STORE_BTN_PIN PB1 // Пин для управление памтью
-#define SCALE_BTN_PIN PB4 // Пин масштабирования изображения
-#define MODE_BTN_PIN  PB5 // Пин режима
+#define STORE_BTN_PIN 	PB1 // Пин для управление памтью
+#define SCALE_BTN_PIN 	PB4 // Пин масштабирования изображения
+#define MODE_BTN_PIN  	PB5 // Пин режима
 
-#define STORE_SIZE    1
+#define STORE_SIZE 		2   // Размер кольцевого буфера памяти
 
 // ===========================================================================
 
+// Класс для работы с таймером, упрощает логику работы с ним
+// сделано для избавления от прерываний потока управления на delay();
 class MilTimer {
 	private:
 		unsigned long tNext = 0, intervalTime;
 
 	public:
+		// MilTimer - констуктор класса, задается интервал срабатывания в мс
 		MilTimer(unsigned long time) : intervalTime(time) {}
+		
+		// isDone - проверка прохождения таймером интервала и обновление счетчика
 		bool isDone() {
 			unsigned long now = millis();
 
@@ -34,28 +40,34 @@ class MilTimer {
 };
 
 // ===========================================================================
+MilTimer time(1500); // Таймер для измерений - 1.5 секунд
 
-// Буфер для экрана
-uint8_t GraphicArray[SCREEN_WIDTH];
-volatile uint8_t scaleFactor = 1;
+// Установки парамтеров кнопок управления ====
+GButton modeBtn(MODE_BTN_PIN);		// Кнопка смены режма работы устиройства
+GButton storeBtn(STORE_BTN_PIN);		// Кнопка работы с памятью
+GButton scaleBtn(SCALE_BTN_PIN);		// Кнопка масштабирования изображения
 
-// Хранилище значений в массиве
-uint8_t SaveArray[STORE_SIZE][SCREEN_WIDTH];
-int SaveArrShift = 0;
+// Параметры отображения ===
+uint8_t scaleFactor = 1; 				// Значение масштабирования графика
+uint8_t GraphicBuffer[SCREEN_WIDTH]; 	// Буфер для экрана в массиве
 
+boolean viewMode = false;	// Режим отображения
+
+// Состояние запроса на сохранение графика.
+// По умолчанию - false, при переключении сохраняется буфер в память и значение сбрасыавется в стоковое
 boolean saveState = false;
-boolean viewMode = false;
 
-MilTimer time(1500); // Таймер для измерений
+// Индекс сдвига по памяти для работы с ее сегментами
+uint8_t memorySaveShift = 0;
 
-// Установки парамтеров кнопок управления
-GButton modeBtn(MODE_BTN_PIN);
-GButton storeBtn(STORE_BTN_PIN);
-GButton scaleBtn(SCALE_BTN_PIN);
+// Переключение секции памяти через кольцевой буфер
+void incrementSaveShift() {memorySaveShift = (memorySaveShift + 1) % STORE_SIZE;}
 
 // ===========================================================================
 
 void setup() {
+	
+	// Инициализируем экран отображения
     oled.begin(
 		128, 64,
 		sizeof(tiny4koled_init_128x64br),
@@ -64,54 +76,83 @@ void setup() {
     oled.clear();
     oled.on();
 
+	// Преднастройки нтервалов для простоты работы и удобства, ничего более
 	modeBtn.setTimeout(500);
 	storeBtn.setTimeout(500);
   	scaleBtn.setTimeout(500);
 }
 
 void loop() {
-	modeBtn.tick();
-	storeBtn.tick();
-	scaleBtn.tick();
-	
-	if (scaleBtn.isSingle()) scaleFactor = scaleFactor + 1; // Масштабируем ихображение
-  	if (scaleBtn.isHold()) scaleFactor = 1; // Сброс масштаба на экране
 
-  	// Смена режима отображения из памяти или чтения канала
-	if (modeBtn.isSingle()) { viewMode = !viewMode; return; }
-  
-	// Переключаем секцию памяти через кольцевой буфер
-	if (modeBtn.isSingle()) { SaveArrShift = (SaveArrShift + 1) % STORE_SIZE; return; }
+    // Обработка кнопок в начале каждого цикла
+    modeBtn.tick();
+    storeBtn.tick();
+    scaleBtn.tick();
 
- 	// Сохраняем уже отображаемый фрейм с графиком в буфер, если стоит режим чтения канала
-  	if (storeBtn.isHold() && !viewMode) { saveState = true; return; }
-	
-  // Проверка на режим чтения памяти, если да, то делаем отображение по запросу
-	if (viewMode) 
-	{
-		if (storeBtn.isSingle()) {
-			readSavedADC(GraphicArray);
-			oledDisplay(GraphicArray, scaleFactor, false);
-		}
-		return;
-	}
+	// ========================
 
-  // Стандартное отображение в графика
-	if (time.isDone())
-	{	
-		if (!saveState) readADC(GraphicArray);
-		oledDisplay(GraphicArray, scaleFactor, saveState);
-    	saveState = false;
-	}
+	// Масштабируем изображение
+    if (scaleBtn.isSingle()) scaleFactor = scaleFactor + 1;
+
+	// Сброс масштаба на экране
+    if (scaleBtn.isHold()) scaleFactor = 1; 
+
+	// Сброс EEPROM
+    if (scaleBtn.isTriple()) for (uint8_t i = 0; i < 128 * STORE_SIZE; i++) EEPROM.write(i, 0);
+
+    // Смена режима отображения из памяти или чтения пробы
+    if (modeBtn.isHold()) {
+        viewMode = !viewMode;
+        return;
+    }
+
+	if (modeBtn.isSingle()) {
+        incrementSaveShift();
+        return;
+    }
+
+    // Сохраняем уже отображаемый фрейм с графиком в буфер, если стоит режим чтения канала
+	// Задается состояние переменной статуса сохранения, сохранение проихсодит на следующем цикле
+	// Сохраняется уже отображенный фрейм графика, не новый
+    if (storeBtn.isHold() && !viewMode) {
+        saveState = true;
+        return;
+    }
+
+	// Проверка на режим чтения памяти, если да, то делаем отображение по запросу
+    if (storeBtn.isSingle() && viewMode) {
+        readADCfromMEM(GraphicBuffer);
+        oledDisplay(GraphicBuffer, scaleFactor);
+    }
+
+	// ========================
+
+    // Стандартное отображение графика
+    if (!viewMode && time.isDone()) {
+        if (saveState) {
+            saveState = false;
+            saveADCtoMEM(GraphicBuffer);
+        } else {
+            readADC(GraphicBuffer);
+            oledDisplay(GraphicBuffer, scaleFactor);
+        }
+    }
 }
 
 // ===========================================================================
 
 // readSavedADC - читает из памяти со сдвигом в буфер для экрана
-void readSavedADC(uint8_t *arr) {
+void readADCfromMEM(uint8_t *arr) {
 	for (uint8_t x = 0; x < SCREEN_WIDTH; x++)
 	{
-		arr[x] = SaveArray[SaveArrShift][x];
+		arr[x] = EEPROM.read(memorySaveShift * 128 + x);
+	}
+}
+
+void saveADCtoMEM(uint8_t *arr) {
+	for (uint8_t x = 0; x < SCREEN_WIDTH; x++)
+	{
+		EEPROM.write(memorySaveShift * 128 + x, arr[x]);
 	}
 }
 
@@ -124,28 +165,31 @@ void readADC(uint8_t *arr) {
 }
 
 // oledDisplay - отобрпажаем массив в память дисплея
-void oledDisplay(uint8_t* valueArr, uint8_t scale, boolean isSave) {
+void oledDisplay(uint8_t* valueArr, uint8_t scale) {
 	oled.clear();
 	for (uint8_t x = 0; x < SCREEN_WIDTH; x++) {
-	
 		uint8_t pos = x / scale;
-		
-    	// Сохрканение отображаемого массива в память по инедксам
-		if (isSave) SaveArray[SaveArrShift][x] = GraphicArray[pos];
-	
-	  	// Вычисление страницы и бита
-		// Ограничиваем значение от 0 до 63
-		uint8_t value = (valueArr[pos] > 63) ? 63 : valueArr[pos];
-		
-	    uint8_t page = value / 8;    			// Определяем страницу (каждая страница 8 пикселей)
-	    uint8_t shiftBit = 1 << (value % 8);   	// Определяем бит в странице
-	
-		// Определеяем страницу пространства экрана
-	    oled.setCursor(x, page);
-	
-		// Устанавливаем курсор в нужную позицию
-	    oled.startData();
-	    oled.sendData(shiftBit);
-	    oled.endData();
+		setOledDot(x, 31);
+	    setOledDot(x, valueArr[pos]);
 	}
+}
+
+// setOledDot - установки точки на нужной координате
+void setOledDot(uint8_t x, uint8_t y) {
+
+	// Ограничиваем значение по высоте отрисовки
+	y = (y > 63) ? 63 : y;
+	
+	// Определяем страницу (каждая страница 8 пикселей)
+	uint8_t page = y / 8;
+	// Определяем бит в странице
+    uint8_t shiftBit = 1 << (y % 8);
+
+	// Устанавливаем страницу пространства экрана
+    oled.setCursor(x, page);
+
+	// Устанавливаем бит в позицию
+    oled.startData();
+    oled.sendData(shiftBit);
+    oled.endData();
 }
